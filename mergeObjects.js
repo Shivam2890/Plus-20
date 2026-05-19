7ca9c73450c1ab15298e03db2fb28908fae15e26
Shivam2890 <Shivamkori2890@gmail.com>
2026-05-19 15:00:02 +0530

    solved

---
+++ mergeObjects.js
@@ -0,0 +1,34 @@
+function mergeObjects(arr) {
+    let result = {}
+    for (let item of arr) {
+        let role = item.name
+        if (!result[role]) {
+            result[role] = item.values
+        } else {
+            result[role].push(...item.values)
+        }
+    }
+    let ans = []
+    for (let key in result) {
+        ans.push({
+            name: key,
+            values: result[key]
+        })
+    }
+    console.log(ans)
+}
+
+// Test Case 1
+console.log(
+    mergeObjects([
+        { name: 'a', values: [1, 2] },
+        { name: 'b', values: [3] },
+        { name: 'a', values: [4, 5] }
+    ])
+)
+
+// Expected Output:
+// [
+//   { name: 'a', values: [1,2,4,5] },
+//   { name: 'b', values: [3] }
+// ]
